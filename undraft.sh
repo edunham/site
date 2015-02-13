2aadb671787293d480809aa072eedca9bcfff09e
edunham <edunham@osuosl.org>
2015-02-13 10:06:47 -0800

    save keystrokes when posting drafts

---
+++ undraft.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if python -c 'import sys; print hasattr(sys, "real_prefix")' | grep -q "False"
+then                                # the above checks if we're in the venv
+    source v/bin/activate
+fi
+if [ ! -f $1 ]                      # make sure we have a file specified
+then
+    echo "Please specify the file to post."
+    exit -1
+fi    
+tinker --post $1                    # tinker gets called on drafts/thing.rst
+git stash save blogscript           # let's not accidentally commit other work
+git rm $1                           # try to remove draft
+git add `date +"%Y/%m/%d/"`${1##*/} # witchcraft to find its posted path
+git commit -m "Publish ${1##*/}"    # and commit, with message
+git stash apply blogscript          # put back any other work
